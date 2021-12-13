b3dc27abddf527ab7536a5c437e451545fa318c4
yumekiti <yumekiti120440@gmail.com>
2021-12-13 19:49:38 +0900

    add: index

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,12 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Index from '../views/Index.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
+    name: 'Index',
+    component: Index
+  },
+  {
+    path: '/home',
     name: 'Home',
     component: Home
   },
---
+++ vue/src/views/Index.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+        <h1 id="title">kusoFORM</h1>
+        <p>
+            フォーム送信できたらゲームクリアです。<br>
+            短時間で送信ができるよう頑張ってください。
+        </p>
+        <v-btn
+            class="no-btn"
+            elevation="2"
+            outlined
+            height="100"
+            rounded
+        >
+            <div>
+                <span class="no-next">次のページへ</span><br>
+                <span class="no">行かない</span>
+            </div>
+        </v-btn>
+        <p class="next">Please <u>click</u> <span @click="next()">HERE</span> to GO to the <span style="color: blue;">next page</span></p>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Index',
+    methods: {
+        next(){
+            this.$router.push('/form')
+        }
+    },
+}
+</script>
+<style scoped>
+
+div{
+    text-align: center;
+}
+
+#title{
+    font-size: 200px;
+    margin: 30px;
+}
+
+.no-btn{
+    margin: 30px;
+}
+
+.no-next{
+    font-size: 40px;
+}
+
+.no{
+    font-size: 1px;
+    color: #ddd;
+}
+
+.next{
+    color: #999;
+    margin: 30px;
+}
+
+</style>
